454e64a4a0d708d73b2d5edaa85445f5d69f00e3
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2021-12-07 21:46:29 +0700

    refactory fake data

--- EUS/app/src/main/java/com/example/eus/ViewHome/HomeFragment.kt
+++ EUS/app/src/main/java/com/example/eus/ViewHome/HomeFragment.kt
@@ -36,14 +36,14 @@ class HomeFragment : Fragment() {
 
 
         adapterCategory = AdapterCategory()
-        fakeCategory().observe(viewLifecycleOwner, Observer {
+        Util.fakeCategory().observe(viewLifecycleOwner, Observer {
             adapterCategory.setData(it)
         })
         binding.recyclerCategory.layoutManager = GridLayoutManager(context,5)
         binding.recyclerCategory.adapter = adapterCategory
 
         adapterProduct = AdapterProduct()
-        fakeData().observe(viewLifecycleOwner, Observer {
+        Util.fakeData().observe(viewLifecycleOwner, Observer {
             adapterProduct.setProduct(it)
         })
 
@@ -70,37 +70,6 @@ class HomeFragment : Fragment() {
         inflater.inflate(R.menu.main_menu, menu)
 
         super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    private fun fakeData() : MutableLiveData<List<Product>> {
-        var mutableLiveData : MutableLiveData<List<Product>> = MutableLiveData()
-        var list = ArrayList<Product>()
-        list.add(Product("https://upload.wikimedia.org/wikipedia/commons/0/0c/E-girl.png",44000000.0,"Điện thoại "))
-        list.add(Product("https://vnn-imgs-a1.vgcloud.vn/icdn.dantri.com.vn/2021/05/08/kimoanh-851-1620472406599.jpeg",23000000.0,"Điện thoại đẹp"))
-        list.add(Product("https://media.istockphoto.com/photos/lonly-young-woman-by-the-sea-picture-id840501418?b=1&k=20&m=840501418&s=170667a&w=0&h=2Smu2ybUDuZB120uKmQeEtegDXCTXHSLd3yL9GouwuQ=",42000000.0,"Điện thoại đẹp"))
-        list.add(Product("https://icdn.dantri.com.vn/thumb_w/640/2021/02/27/diem-danh-7-guong-mat-hot-girl-xinh-dep-noi-bat-trong-thang-2-docx-1614441453135.jpeg",55000000.0,"Điện thoại đẹp"))
-        list.add(Product("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk3O6wPUnvDOyGr-8Wbd6-lihTBhXQNfeCyg&usqp=CAU",20000000.0,"Điện thoại đẹp"))
-        mutableLiveData.value = list
-        return mutableLiveData
-    }
-
-    private fun fakeCategory() : MutableLiveData<List<String>> {
-        var mutableLiveData : MutableLiveData<List<String>> = MutableLiveData()
-        var list = ArrayList<String>()
-
-        list.add("Tivi")
-        list.add("Tủ Lạnh")
-        list.add("Bàn Thờ")
-        list.add("Điện Thoại")
-        list.add("Laptop")
-        list.add("Cpu")
-        list.add("Gpu")
-        list.add("Máy ")
-        list.add("Tablet")
-        list.add("Bao Cao Su")
-
-        mutableLiveData.value = list
-        return mutableLiveData
     }
 
 }
--- EUS/app/src/main/java/com/example/eus/ViewHome/Util.kt
+++ EUS/app/src/main/java/com/example/eus/ViewHome/Util.kt
@@ -1,5 +1,7 @@
 package com.example.eus.ViewHome
 
+import androidx.lifecycle.MutableLiveData
+import com.example.eus.ODT.Product
 import java.text.NumberFormat
 
 class Util {
@@ -14,6 +16,37 @@ class Util {
                 }
             }
             return number
+        }
+
+        fun fakeCategory() : MutableLiveData<List<String>> {
+            var mutableLiveData : MutableLiveData<List<String>> = MutableLiveData()
+            var list = ArrayList<String>()
+
+            list.add("Tivi")
+            list.add("Tủ Lạnh")
+            list.add("Bàn Thờ")
+            list.add("Điện Thoại")
+            list.add("Laptop")
+            list.add("Cpu")
+            list.add("Gpu")
+            list.add("Máy ")
+            list.add("Tablet")
+            list.add("Bao Cao Su")
+
+            mutableLiveData.value = list
+            return mutableLiveData
+        }
+
+        fun fakeData() : MutableLiveData<List<Product>> {
+            var mutableLiveData : MutableLiveData<List<Product>> = MutableLiveData()
+            var list = ArrayList<Product>()
+            list.add(Product("https://upload.wikimedia.org/wikipedia/commons/0/0c/E-girl.png",44000000.0,"Điện thoại "))
+            list.add(Product("https://vnn-imgs-a1.vgcloud.vn/icdn.dantri.com.vn/2021/05/08/kimoanh-851-1620472406599.jpeg",23000000.0,"Điện thoại đẹp"))
+            list.add(Product("https://media.istockphoto.com/photos/lonly-young-woman-by-the-sea-picture-id840501418?b=1&k=20&m=840501418&s=170667a&w=0&h=2Smu2ybUDuZB120uKmQeEtegDXCTXHSLd3yL9GouwuQ=",42000000.0,"Điện thoại đẹp"))
+            list.add(Product("https://icdn.dantri.com.vn/thumb_w/640/2021/02/27/diem-danh-7-guong-mat-hot-girl-xinh-dep-noi-bat-trong-thang-2-docx-1614441453135.jpeg",55000000.0,"Điện thoại đẹp"))
+            list.add(Product("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk3O6wPUnvDOyGr-8Wbd6-lihTBhXQNfeCyg&usqp=CAU",20000000.0,"Điện thoại đẹp"))
+            mutableLiveData.value = list
+            return mutableLiveData
         }
     }
 }
