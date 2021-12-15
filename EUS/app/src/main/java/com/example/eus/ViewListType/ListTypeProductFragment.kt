fe1bfd0f99efcd1a9e6eddfea7137db61bf1b734
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2021-12-15 15:34:07 +0700

    Check

--- EUS/app/src/main/java/com/example/eus/ViewListType/ListTypeProductFragment.kt
+++ EUS/app/src/main/java/com/example/eus/ViewListType/ListTypeProductFragment.kt
@@ -1,16 +1,20 @@
 package com.example.eus.ViewListType
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.eus.R
+import com.example.eus.ViewDetail.DetailFragmentArgs
 import com.example.eus.ViewHome.AdapterProduct
 import com.example.eus.ViewHome.Util
+import com.example.eus.ViewModel.EUSViewModel
 import com.example.eus.databinding.FragmentListTypeProductBinding
 
 
@@ -18,6 +22,8 @@ class ListTypeProductFragment : Fragment() {
 
     private lateinit var binding : FragmentListTypeProductBinding
     private lateinit var adapterProduct: AdapterProduct
+    private lateinit var viewModel : EUSViewModel
+    private val args: ListTypeProductFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,8 +35,10 @@ class ListTypeProductFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(EUSViewModel::class.java)
         adapterProduct = AdapterProduct()
-        Util.fakeData().observe(viewLifecycleOwner, Observer {
+        viewModel.getListProduct(args.title).observe(viewLifecycleOwner, Observer {
             adapterProduct.setProduct(it)
         })
         binding.recyclerListProduct.adapter = adapterProduct
